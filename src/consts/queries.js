4e5923c30ae1a9f533c56d28459381eee8b4055f
slevinger <shir.levinger@gmail.com>
2020-06-19 22:01:31 +0300

    switching to env variables and creating consts

--- src/consts/queries.js
+++ src/consts/queries.js
@@ -1,11 +1,10 @@
 const insertMessageAndUserToTable = (username, message) =>
   `INSERT into \`messages\` (\`username\`,\`message\`,\`sent_at\`) VALUES('${username}','${message}','${new Date().getTime()}')`;
 
-const getLastTenMessagesFromTable = () =>
+const getLastTenMessagesFromTable =
   "SELECT `id`, `username`, `message`, `sent_at` FROM `messages` WHERE 1=1 order by id desc limit 10";
 
-const removeAllButLast50Messages = () => {
-  return `delete from \`messages\` WHERE \`id\` in (
+const removeAllButLast50Messages = `delete from \`messages\` WHERE \`id\` in (
     SELECT \`id\`  from (
         SELECT @rownum:=@rownum+1 rownum, 
         t.* 
@@ -16,7 +15,6 @@ const removeAllButLast50Messages = () => {
     ) tt 
     where 1=1 
     and tt.rownum > 100)`;
-};
 
 module.exports = {
   insertMessageAndUserToTable,
